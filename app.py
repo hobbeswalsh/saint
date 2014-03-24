29e9e9b47e9006e0a247a3a22ab398e8e670ba66
Robin Walsh <walrobin@amazon.com>
2014-03-24 12:40:33 -0700

    next clue

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def serve_index():
 def serve_index1():
     return render_template("index2.html")
 
+@app.route("/2")
+def serve_index2():
+    return render_template("index3.html")
+
 
 def main():
     port = os.environ.get("PORT", 5555)
